097fc82a76b3ce253e584c2de4a42c534d08ef9b
candresom <cocana@sodimac.vc-soft.com>
2023-12-09 11:10:35 -0500

    UPDATE-SGIP - Actualización de título

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sgip';
+  title = 'Sistema de Gestión de Información de posgrados';
 }
