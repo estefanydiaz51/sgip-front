5f0224d9883f532300bd8ece7e7446be3985b469
candresom <cocana@sodimac.vc-soft.com>
2023-12-13 14:39:03 -0500

    UPDATE- Ajustes en formulario de creación de programas

--- src/app/modules/programs/components/program-create-form/program-create-form.component.ts
+++ src/app/modules/programs/components/program-create-form/program-create-form.component.ts
@@ -75,15 +75,15 @@ export class ProgramCreateFormComponent implements OnInit {
 
   }
 
-  handleChange(info: NzUploadChangeParam): void {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      this.msg.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      this.msg.error(`${info.file.name} file upload failed.`);
-    }
-  }
+  // handleChange(info: NzUploadChangeParam): void {
+  //   if (info.file.status !== 'uploading') {
+  //     console.log(info.file, info.fileList);
+  //   }
+  //   if (info.file.status === 'done') {
+  //     this.msg.success(`${info.file.name} file uploaded successfully`);
+  //   } else if (info.file.status === 'error') {
+  //     this.msg.error(`${info.file.name} file upload failed.`);
+  //   }
+  // }
 
  }
